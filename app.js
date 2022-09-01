20b1fd889dd5aaf3076350b44d132c859cf4874c
rudyjm3 <rudyjm3@yahoo.com>
2022-09-01 16:44:18 -0400

    -Added time out function to get attruibute data-delay from the fading elements to set time of animation. -Stop observing elements once they have been observered the 1st time once in the view port.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const fadeObserver = new IntersectionObserver(
    (elsToFade) => {
       elsToFade.forEach((el) => {
+         console.log(el);
          if (el.isIntersecting) {
-            el.target.classList.add('faded');
+            setTimeout(() => {
+               el.target.classList.add('faded');
+            }, +el.target.getAttribute('data-delay') || 0);
+            //stop observering elements after the function has ran for the 1st time
+            fadeObserver.unobserve(el.target);
          }
       })
    }
